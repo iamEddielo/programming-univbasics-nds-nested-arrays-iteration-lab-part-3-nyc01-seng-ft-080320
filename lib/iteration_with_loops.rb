3d3bf660d8d201fcc7fd7e122d9a98d1b5c18c20
Eddie Lopez <iameddielopez@gmail.com>
2020-07-20 16:48:02 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@ def join_nested_strings(src)
 
   while row_index < src.count do 
     element_index = 0
-    binding pry
+    binding 'pry'
     while element_index < src[row_index].count do 
       
       if src[row_index][element_index].class == String
